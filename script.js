ae8a315068836f40a3603098b4f9bdf987a8cf35
Leo <leodasilva.2012@hotmail.com>
2022-05-04 23:32:11 -0300

    add ui

--- script.js
+++ script.js
@@ -1,3 +1,69 @@
+let playerScore = 0;
+let computerScore = 0;
+const playerScore_span = document.getElementById("player-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("rock");
+const paper_div = document.getElementById("paper");
+const scissors_div = document.getElementById("scissors");
+const message_p = document.querySelector(".message > p");
+
+function win(textWin) {
+    playerScore++;
+    playerScore_span.innerHTML = playerScore;
+    computerScore_span.innerHTML = computerScore;
+    result_p.innerHTML = textWin;
+    message_p.innerHTML = " — BEGINNER'S LUCK!";
+
+}
+
+function lost(textLost) {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    playerScore_span.innerHTML = playerScore;
+    result_p.innerHTML = textLost;
+    message_p.innerHTML = " — I'M THE BEST!";
+
+}
+
+function draw(textDraw) {
+    result_p.innerHTML = textDraw;
+
+}
+
+function theWinner() {
+    if (computerScore >= 5) {
+        computerScore = 0;
+        playerScore = 0;
+        computerScore_span.innerHTML = 0;
+        playerScore_span.innerHTML = 0;
+        result_p.innerHTML = "GAME OVER. COMPUTER IS THE WINNER!"
+        message_p.innerHTML = " — BETTER LUCK NEXT TIME!"
+    }
+    if (playerScore >= 5) {
+        computerScore = 0;
+        playerScore = 0;
+        computerScore_span.innerHTML = 0;
+        playerScore_span.innerHTML = 0;
+        result_p.innerHTML = "GAME OVER. PLAYER IS THE WINNER!"
+        message_p.innerHTML = " — YOU WON. LET'S PLAY AGAIN?"
+    }
+}
+
+function playerSelection() {
+    rock_div.addEventListener('click', function() {
+        playRound("rock");
+    });
+    paper_div.addEventListener('click', function(e) {
+        playRound("paper");
+    });
+    scissors_div.addEventListener('click', function(e) {
+        playRound("scissors");
+
+    });
+}
+
 function computerPlay() {
     const computerSelection = Math.floor(Math.random() * 3);
     switch (computerSelection) {
@@ -10,50 +76,39 @@ function computerPlay() {
     }
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection == "rock" && computerSelection == "scissors") {
-        ++playerScore
-        return "Rock beats Scissors. Player wins!"
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        ++playerScore
-        return "Scissors beats Paper. Player wins!"
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        ++playerScore
-        return "Paper beats Rock. Player wins!"
-    } else if (computerSelection == "rock" && playerSelection == "scissors") {
-        ++computerScore
-        return "Rock beats Scissors. Computer wins!"
-    } else if (computerSelection == "scissors" && playerSelection == "paper") {
-        ++computerScore
-        return "Scissors beats Paper. Computer wins!"
-    } else if (computerSelection == "paper" && playerSelection == "rock") {
-        ++computerScore
-        return "Paper beats Rock. Computer wins!"
-    } else {
-        return "It's a tie!"
+function playRound(playerSelection) {
+    computerSelection = computerPlay();
+    let computerWin = false;
+    if (computerSelection == playerSelection) {
+        draw("YOU BOTH CHOSE THE SAME. IT'S A DRAW.")
+        return
+
     }
-}
-let playerScore = 0
-let computerScore = 0
+    if (playerSelection == "rock") {
+        if (computerSelection == "paper") {
+            computerWin = true
+            lost(`${computerSelection} beats ${playerSelection}.Computer wins!`)
 
-function game() {
-
-    for (let i = 0; i < 5; i++) {
-
-        let playerSelection = prompt("Rock, Paper Or Scissors?").toLowerCase();
-
-        console.log(playRound(playerSelection, computerPlay()))
-        console.log("Player score: " + playerScore)
-        console.log("Computer score: " + computerScore)
+        }
     }
-    if (playerScore > computerScore) {
-        console.log("Player won " + playerScore + " of 5 rounds. Is the Winner.")
-    } else if (computerScore > playerScore) {
-        console.log("Computer won " + computerScore + " of 5 rounds. Is the Winner.")
-    } else {
-        console.log("No more rounds. " + "Player " + playerScore + "." + " Computer " + computerScore + ". It's a tie.")
+    if (playerSelection == "scissors") {
+        if (computerSelection == "rock") {
+            computerWin = true
+            lost(`${computerSelection} beats ${playerSelection}. Computer wins!`)
+
+        }
     }
+    if (playerSelection == "paper") {
+        if (computerSelection == "scissors") {
+            computerWin = true
+            lost(`${computerSelection} beats ${playerSelection}. Computer wins!`)
+
+        }
+    }
+    if (!computerWin) {
+        win(`${playerSelection} beats ${computerSelection}. Player Wins!`)
+    }
+    theWinner()
 }
 
-
-game()
+playerSelection()
